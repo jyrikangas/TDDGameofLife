e69f85b561025957f3ba7b50eff20349c6d571dd
Jyri Kangas <jyri_kangas@outlook.com>
2023-03-11 15:15:37 +0200

    game can be created and it parses a state string into a board state

--- src/example.mjs
+++ src/example.mjs
@@ -1,3 +1,63 @@
-export function sum(a, b) {
-  return a + b;
+
+
+export class Game {
+  width;
+  height;
+  board;
+  constructor(width, height, state) {
+    this.width = width;
+    this.height = height;
+    this.board = new Array(height)
+    for(let i = 0; i < height; i++) {
+      this.board[i] = new Array(width);
+    }
+    this.state = state || ""
+    this.parse();
+  }
+
+  parse() {
+    let pointer = 0;
+    let lineEnd= false;
+    let number = 0;
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        console.log(i,j,pointer, this.state[pointer]);
+        
+        if (this.state[pointer] === "!") {
+          console.log(this.board);
+          return;
+        }
+        if (number > 0) {
+          this.board[i][j] = this.state[pointer] === "o";
+          number--;
+          if (number === 0) {
+            pointer++;
+          }
+        } else if (lineEnd) {
+          this.board[i][j] = false;
+        } else if (this.state[pointer] === "$") {
+          console.log(i, j, "line end");
+
+          this.board[i][j] = false;
+          console.log(this.board);
+          lineEnd = true;
+        } else if (this.state[pointer] === "b" || this.state[pointer] === "o") {
+        this.board[i][j] = this.state[pointer] === "o";
+        pointer++;
+        } else {
+          number = Number (this.state[pointer]);
+          pointer++;
+          this.board[i][j] = this.state[pointer] === "o";
+          number--;
+        }
+          
+      }
+      lineEnd = false;
+      pointer++;
+
+    }
+  }
+  toString() {
+    return this.state
+  }
 }
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,8 +1,19 @@
 import { expect } from "chai";
-import { sum } from "../src/example.mjs";
+import { Game } from "../src/example.mjs";
 
-describe("Example test fixture", () => {
-  it("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
+describe("Game", () => {
+  it("can be created", () => {
+    let game = new Game(5, 5);
+    expect(game.toString()).to.equal("");
+  });
+
+  it("can be created with a state", () => {
+    let game = new Game(3, 3, "bo$2bo$3o!")
+    expect(game.toString()).to.equal("bo$2bo$3o!");
+  });
+
+  it("parses the state into the board", () => {
+    let game = new Game(3, 3, "bo$2bo$3o!")
+    expect(game.board).to.deep.equal([[false, true, false], [false, false, true], [true, true, true]]);
   });
 });
