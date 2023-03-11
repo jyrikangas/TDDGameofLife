bdc480821974ba4e66d679ddca86bf3be0c3e0bc
Jyri Kangas <jyri_kangas@outlook.com>
2023-03-11 16:15:35 +0200

    can simulate a tick of the game

--- src/example.mjs
+++ src/example.mjs
@@ -9,7 +9,7 @@ export class Game {
     this.height = height;
     this.board = new Array(height)
     for(let i = 0; i < height; i++) {
-      this.board[i] = new Array(width);
+      this.board[i] = new Array(width).fill(false);
     }
     this.state = state || ""
     this.parse();
@@ -21,41 +21,70 @@ export class Game {
     let number = 0;
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        console.log(i,j,pointer, this.state[pointer]);
-        
         if (this.state[pointer] === "!") {
-          console.log(this.board);
           return;
         }
+        if (this.state[pointer] === "$") {
+          break;
+        }
+
         if (number > 0) {
           this.board[i][j] = this.state[pointer] === "o";
           number--;
           if (number === 0) {
             pointer++;
           }
-        } else if (lineEnd) {
-          this.board[i][j] = false;
-        } else if (this.state[pointer] === "$") {
-          console.log(i, j, "line end");
-
-          this.board[i][j] = false;
-          console.log(this.board);
-          lineEnd = true;
         } else if (this.state[pointer] === "b" || this.state[pointer] === "o") {
-        this.board[i][j] = this.state[pointer] === "o";
-        pointer++;
+          this.board[i][j] = this.state[pointer] === "o";
+          pointer++;
         } else {
           number = Number (this.state[pointer]);
           pointer++;
           this.board[i][j] = this.state[pointer] === "o";
           number--;
         }
-          
       }
       lineEnd = false;
       pointer++;
 
     }
+  }
+
+  tick() {
+    let newBoard = new Array(this.height)
+    let neighbours = 0;
+    for(let i = 0; i < this.height; i++) {
+      newBoard[i] = new Array(this.width).fill(false);
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        let lastRow = i-1>0?i-1:0;
+        let nextRow = i+2<this.height?i+2:this.height;
+        let lastCol = j-1>0?j-1:0;
+        let nextCol = j+2<this.width?j+2:this.width;
+
+        for (let ii = lastRow; ii < nextRow; ii++) {
+          for (let jj = lastCol; jj < nextCol; jj++){
+            if (this.board[ii][jj]){
+              neighbours++;
+            }
+          }
+        }
+
+        if (this.board[i][j]) {
+          if (neighbours>1 && neighbours<5) {
+            newBoard[i][j]=true;
+          }
+
+        } else {
+          if (neighbours===3) {
+            newBoard[i][j]=true;
+          }
+        }
+        neighbours = 0;
+      }
+    }
+    this.board=newBoard;
   }
   toString() {
     return this.state
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -16,4 +16,38 @@ describe("Game", () => {
     let game = new Game(3, 3, "bo$2bo$3o!")
     expect(game.board).to.deep.equal([[false, true, false], [false, false, true], [true, true, true]]);
   });
+  
+});
+
+describe("after a tick of the game ", () => {
+  it("A dead board remains dead", () => {
+    let game = new Game(3, 3, "!")
+    game.tick();
+    expect(game.board).to.deep.equal([[false, false, false], [false, false, false], [false, false, false]]);
+  });
+
+  it("a board with 1 living cell has become a dead board", () => {
+    let game = new Game(3, 3, "$bo$!")
+    game.tick();
+    expect(game.board).to.deep.equal([[false, false, false], [false, false, false], [false, false, false]]);
+  });
+
+  it("a board with 2 living cells next to each other still has 2 living cells", () => {
+    let game = new Game(3, 3, "$b2o$!")
+    game.tick();
+    expect(game.board).to.deep.equal([[false, false, false], [false, true, true], [false, false, false]]);
+  });
+
+  it("a board with 3 living cells neighboring a dead cell has a fourth live cell be born", () => {
+    let game = new Game(3, 3, "2bo$b2o$!")
+    game.tick();
+    expect(game.board).to.deep.equal([[false, true, true], [false, true, true], [false, false, false]]);
+  });
+
+  it("a live cell with more than 3 live neighbours dies", () => {
+    let game = new Game(3, 3, "3o$2o$!")
+    game.tick();
+    expect(game.board).to.deep.equal([[true, false, true], [true, false, true], [false, false, false]]);
+  });
+
 });
